c62f829ba0831ded2cba6614ea9ae5289711d0b7
xuesheng <xues1991@163.com>
2017-01-24 13:27:41 +0800

    初始提交

---
+++ tests/RouteCollectorTest.php
@@ -0,0 +1,31 @@
+<?php
+namespace Xuesheng\Route\Test;
+use Xuesheng\Route;
+
+class RouteCollectorTest extends \PHPUnit_Framework_TestCase
+{
+    public function testSetsDoctorWithConstructor()
+    {
+        $route = new Route\Route('Lisi');
+        $this->assertAttributeEquals('Lisi', 'favoriteDoctor', $route);
+    }
+
+    public function testSaysDoctorName()
+    {
+        $route = new Route\Route('Lisi');
+        $this->assertEquals('The best doctor is Lisi', $route->say());
+    }
+
+    public function testRespondToInAgreement()
+    {
+        $route = new Route\Route('Lisi');
+        $this->assertEquals('I agree!', $route->respondTo('Lisi is the best doctor'));
+    }
+
+    public function testRespondToInDisagreement()
+    {
+        $route = new Route\Route('Lisi');
+        $route->respondTo('No way');
+    }
+
+}
